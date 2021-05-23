3bb3fea861fe2a00c60d28c25170eac53a882083
johnjus <43526050+johnjus@users.noreply.github.com>
2021-05-23 02:43:16 -0500

    Add files via upload

---
+++ Crypto_Tracker/Crypto Tracker/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Crypto_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.onBT = new System.Windows.Forms.Button();
+            this.offBt = new System.Windows.Forms.Button();
+            this.coinDataTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // onBT
+            // 
+            this.onBT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.onBT.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.onBT.Location = new System.Drawing.Point(12, 12);
+            this.onBT.Name = "onBT";
+            this.onBT.Size = new System.Drawing.Size(167, 112);
+            this.onBT.TabIndex = 0;
+            this.onBT.Text = "ON";
+            this.onBT.UseVisualStyleBackColor = false;
+            this.onBT.Click += new System.EventHandler(this.onBT_Click);
+            // 
+            // offBt
+            // 
+            this.offBt.BackColor = System.Drawing.Color.Red;
+            this.offBt.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.offBt.Location = new System.Drawing.Point(1126, 680);
+            this.offBt.Name = "offBt";
+            this.offBt.Size = new System.Drawing.Size(167, 112);
+            this.offBt.TabIndex = 1;
+            this.offBt.Text = "OFF";
+            this.offBt.UseVisualStyleBackColor = false;
+            this.offBt.Click += new System.EventHandler(this.offBt_Click);
+            // 
+            // coinDataTB
+            // 
+            this.coinDataTB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coinDataTB.Location = new System.Drawing.Point(12, 130);
+            this.coinDataTB.Multiline = true;
+            this.coinDataTB.Name = "coinDataTB";
+            this.coinDataTB.ReadOnly = true;
+            this.coinDataTB.Size = new System.Drawing.Size(1281, 544);
+            this.coinDataTB.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.onBT;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.CancelButton = this.offBt;
+            this.ClientSize = new System.Drawing.Size(1305, 804);
+            this.Controls.Add(this.coinDataTB);
+            this.Controls.Add(this.offBt);
+            this.Controls.Add(this.onBT);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crypto Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button onBT;
+        private System.Windows.Forms.Button offBt;
+        private System.Windows.Forms.TextBox coinDataTB;
+    }
+}
+
---
+++ Crypto_Tracker/Crypto Tracker/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using HtmlAgilityPack;
+
+
+
+namespace Crypto_Tracker
+{
+    public partial class Form1 : Form
+    {   
+        // url : https://www.coingecko.com/en/coins/
+        
+
+        private List<Coin> coins = new List<Coin>();
+        private DateTime today;
+        private double totalProfit = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //coin object because c#
+        public class Coin
+        {
+            string coinName;         
+            double coinCurrentPrice;
+            double totalCoinCount;
+            int intialTotalCoinCountValue;
+            
+            public Coin(string coinName, double coinCurrentPrice, double totalCoinCount, int intialTotalCoinCountValue)
+            {
+                this.coinName = coinName; 
+                this.coinCurrentPrice = coinCurrentPrice;
+                this.intialTotalCoinCountValue = intialTotalCoinCountValue;
+                this.totalCoinCount = totalCoinCount;
+            }
+            public string getName()
+            {
+                return this.coinName;
+            }
+            public double getCurrentPrice()
+            {
+                return this.coinCurrentPrice;
+            }
+            public double getTotalCoins()
+            {
+                return this.totalCoinCount;
+            }
+            public int getintialTotalCoinCountValue()
+            {
+                return this.intialTotalCoinCountValue;
+            }
+            public double getCurrentTotalCoinCountValue()
+            {
+                return (Math.Round((this.coinCurrentPrice * this.totalCoinCount), 2));
+            }
+        }
+
+        //Add break points
+        public string CryptoScrapper(string x)
+        {
+            string src = "https://www.coingecko.com/en/coins/";
+            try
+            {
+                HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = web.Load(src + x);
+                try
+                {
+                    var price = doc.DocumentNode.SelectNodes("//span[@class='no-wrap']").ToList();
+                    foreach (var item in price)
+                    {
+                        //Console.WriteLine(x.ToUpper() + ": " + item.InnerText);
+                        return item.InnerText.ToString();
+                        
+                    }              
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Failed to acces the price of: " + x + e);
+                    
+                    return x.ToUpper() + "0";
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("not page found : " + (src+x) + e);
+                return x.ToUpper() + "0"; 
+            }
+            return x.ToUpper() + "0";
+        }
+        public void PopulateObjectCoin( string[] coinNames)
+        {
+            try
+            {
+                for (int i = 0; i < coinNames.Length - 1; i += 3)
+                {
+
+                    //Console.WriteLine(coinNames[i] + " " + );
+                    coins.Add(new Coin(coinNames[i], Convert.ToDouble(CryptoScrapper(coinNames[i]).Trim().Trim('$')), Convert.ToDouble(coinNames[i + 1]), Convert.ToInt32(coinNames[i + 2])));
+                }
+            }
+            catch(Exception e)
+            {
+                Console.Write("Failed to 'PopulateObjectCoin'" + e.ToString());
+            }
+        }
+
+        //this retrieves data of the coin
+        public string[] RetieveCoinsFromDoc()
+        {
+            try
+            {
+                string incomingLine;
+                string builtLine;
+                StreamReader sr = new StreamReader(File location of where your coin data is stored);
+                incomingLine = sr.ReadLine();
+                builtLine = incomingLine;
+                while (incomingLine != null)
+                {
+                    incomingLine = sr.ReadLine();
+                    builtLine += incomingLine;
+                }
+
+                sr.Close();
+
+                string[] fromDocCoinDataSplit = builtLine.Split(':');
+
+                return fromDocCoinDataSplit;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to retrive coin data from document. Exception: " + e.Message);
+                return null;
+            }
+        }
+
+        public void WriteCoinDatatoDoc()
+        {
+            try
+            {
+
+                StreamWriter sw =  new StreamWriter(File location of where you want to store the computed coin data, true);
+                
+
+                sw.WriteLine(today.ToString());
+                foreach (Coin coin in coins)
+                {
+                    totalProfit += Math.Round((coin.getCurrentTotalCoinCountValue() - coin.getintialTotalCoinCountValue()), 2);
+                    sw.WriteLine(coin.getName().ToUpper() + ": Price: $" + coin.getCurrentPrice() + " Amount: " + coin.getTotalCoins() +  " Total Intial Value: $" + coin.getintialTotalCoinCountValue() +" Total Current Value: $"+  coin.getCurrentTotalCoinCountValue() + " Profit: $" + Math.Round((coin.getCurrentTotalCoinCountValue() - coin.getintialTotalCoinCountValue()), 2));
+                    WriteCoinDataToPrompt((coin.getName().ToUpper() + ": Price: $" + coin.getCurrentPrice() + " Amount: " + coin.getTotalCoins() + " Total Intial Value: $" + coin.getintialTotalCoinCountValue() + " Total Current Value: $" + coin.getCurrentTotalCoinCountValue() + " Profit: $" + Math.Round((coin.getCurrentTotalCoinCountValue() - coin.getintialTotalCoinCountValue()), 2)).ToString());
+                }
+                sw.WriteLine("Total Profit: $"  + totalProfit);
+                WriteCoinDataToPrompt("Total Profit: $" + totalProfit);
+                sw.WriteLine(".");
+                coins.Clear();
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+        public void WriteCoinDataToPrompt(string coinData)
+        {
+            try
+            {
+                coinDataTB.AppendText(coinData);
+                coinDataTB.AppendText(Environment.NewLine);
+               
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Failed to populate prompt. Exception: " + e);
+            }
+
+
+        }
+
+        
+        private void onBT_Click(object sender, EventArgs e)
+        {
+            today = DateTime.Now;
+            Console.WriteLine(today.ToString());
+            PopulateObjectCoin(RetieveCoinsFromDoc());
+            WriteCoinDatatoDoc();
+
+
+        }
+
+        private void offBt_Click(object sender, EventArgs e)
+        {
+            
+            Application.Exit();
+        }
+    }
+}
+
